fce5871c4a3f1a288779ff7e3dbaa9662f4c95e0
Joshua Olutunde <joshuafavour81@gmail.com>
2023-05-10 07:08:22 -0700

    unit and e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,35 @@ import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Books } from './entity/books';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database:  'book',
-      entities: [Books],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Books],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'password',
+    //   database:  'book',
+    //   entities: [Books],
+    //   synchronize: true,
+    // }),
     BooksModule],
   controllers: [],
   providers: [],
